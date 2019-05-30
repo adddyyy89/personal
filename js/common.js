e3dc4371363537373e765cfbdbff62feaa0a72a2
akg <akg.bit@gmail.com>
2019-05-30 09:42:00 +0530

    Links & Jquery

---
+++ js/common.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+
+    $("#resume").click(function(){
+        $("#bodyplaceholder").html("<img href='./images/joycemeyer1.jpg' class='fit-in-div' alt='Have Patience..'></img>");
+    });
+    
+    $("#quotes").click(function(){
+        $("#bodyplaceholder").html("<img href='./images/ralphwaldoemerson1.jpg' class='fit-in-div' alt='Have Patience..'></img>");
+    });
+    
+    $("#blogs").click(function(){
+        $("#bodyplaceholder").html("<img href='./images/saadi1.jpg' class='fit-in-div' alt='Have Patience..'></img>");
+    });
+
+});
